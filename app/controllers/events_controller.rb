ac217c0205fd63077f0a9e0b80d5382e098b490b
toshiemon18 <st12318@gmail.com>
2016-03-08 02:31:10 +0900

    イベント編集機能を実装した

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,6 +19,19 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def edit 
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params) then 
+      redirect_to @event, notice: "イベント情報を更新しました"
+    else 
+      render :edit
+    end
+  end
+
   private 
   
   def event_params
